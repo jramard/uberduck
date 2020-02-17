ebd86ee242691586ad9b486f6418f379c0674699
Julien Ramard <julien.ramard@gmail.com>
2020-02-17 20:26:18 +0100

    refactor: adding products to basket

--- src/app/products/products.page.ts
+++ src/app/products/products.page.ts
@@ -7,12 +7,26 @@ import { AngularFireDatabase } from '@angular/fire/database';
   styleUrls: ['./products.page.scss'],
 })
 export class ProductsPage implements OnInit {
-  // public store: any;
+  name: string;
+  rating: string;
+  type: string;
+  price: string;
+  deliveryTime: string;
+  distance: string;
+  imgName: string;
+  products: [];
 
   constructor(public afDB: AngularFireDatabase) {
-    // this.afDB.list('/fast-foods').valueChanges().subscribe((data) => {
-    //   this.store = data[0];
-    // });
+    this.afDB.list('/fast-foods').valueChanges().subscribe((data: any) => {
+      this.name = data[0].name;
+      this.rating = data[0].rating;
+      this.type = data[0].type;
+      this.price = data[0].price;
+      this.deliveryTime = data[0].deliveryTime;
+      this.distance = data[0].distance;
+      this.imgName = data[0].imgName;
+      this.products = data[0].products;
+    });
   }
 
   categories = [
@@ -28,98 +42,103 @@ export class ProductsPage implements OnInit {
   activeCategory = 'Burgers';
 
   basketItems = [
-    {
-      name: 'Classic',
-      price: 10.90,
-      quantity: 1,
-      imgName: 'classic'
-    },
-    {
-      name: 'Wallace',
-      price: 10.90,
-      quantity: 1,
-      imgName: 'wallace'
-    },
-    {
-      name: 'Neruda',
-      price: 10.90,
-      quantity: 1,
-      imgName: 'neruda'
-    },
+    // {
+    //   name: 'Classic',
+    //   price: 10.90,
+    //   quantity: 1,
+    //   imgName: 'classic'
+    // },
+    // {
+    //   name: 'Wallace',
+    //   price: 10.90,
+    //   quantity: 1,
+    //   imgName: 'wallace'
+    // },
+    // {
+    //   name: 'Neruda',
+    //   price: 10.90,
+    //   quantity: 1,
+    //   imgName: 'neruda'
+    // },
   ];
 
-  store = {
-    name: 'Rosaparks',
-    rating: '4,7',
-    type: 'fast-food',
-    price: '$$',
-    deliveryTime: '20-30',
-    distance: '0,8',
-    imgName: 'rosaparks',
-    products: [
-      {
-        name: 'Classic',
-        description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
-        price: 10.90,
-        imgName: 'classic'
-      },
-      {
-        name: 'Wallace',
-        description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
-        price: 10.90,
-        imgName: 'wallace'
-      },
-      {
-        name: 'Neruda',
-        description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
-        price: 10.90,
-        imgName: 'neruda'
-      },
-      {
-        name: 'Nine',
-        description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
-        price: 10.90,
-        imgName: 'nine'
-      },
-      {
-        name: 'Gassan',
-        description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
-        price: 10.90,
-        imgName: 'gassan'
-      },
-      {
-        name: 'Protest',
-        description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
-        price: 10.90,
-        imgName: 'protest'
-      },
-      {
-        name: 'Luther',
-        description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
-        price: 10.90,
-        imgName: 'luther'
-      },
-      {
-        name: 'Little',
-        description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
-        price: 9.90,
-        imgName: 'little'
-      },
-      {
-        name: 'Rosa',
-        description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
-        price: 9.90,
-        imgName: 'rosa'
-      }
-    ]
+  showBasket = false;
+
+  addToBasket(item) {
+    this.showBasket = true;
+    this.basketItems.push(this.products[item]);
   };
+
+  // store = {
+  //   name: 'Rosaparks',
+  //   rating: '4,7',
+  //   type: 'fast-food',
+  //   price: '$$',
+  //   deliveryTime: '20-30',
+  //   distance: '0,8',
+  //   imgName: 'rosaparks',
+  //   products: [
+  //     {
+  //       name: 'Classic',
+  //       description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
+  //       price: 10.90,
+  //       imgName: 'classic'
+  //     },
+  //     {
+  //       name: 'Wallace',
+  //       description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
+  //       price: 10.90,
+  //       imgName: 'wallace'
+  //     },
+  //     {
+  //       name: 'Neruda',
+  //       description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
+  //       price: 10.90,
+  //       imgName: 'neruda'
+  //     },
+  //     {
+  //       name: 'Nine',
+  //       description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
+  //       price: 10.90,
+  //       imgName: 'nine'
+  //     },
+  //     {
+  //       name: 'Gassan',
+  //       description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
+  //       price: 10.90,
+  //       imgName: 'gassan'
+  //     },
+  //     {
+  //       name: 'Protest',
+  //       description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
+  //       price: 10.90,
+  //       imgName: 'protest'
+  //     },
+  //     {
+  //       name: 'Luther',
+  //       description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
+  //       price: 10.90,
+  //       imgName: 'luther'
+  //     },
+  //     {
+  //       name: 'Little',
+  //       description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
+  //       price: 9.90,
+  //       imgName: 'little'
+  //     },
+  //     {
+  //       name: 'Rosa',
+  //       description: 'Bœuf bio ● comté affiné 18 mois ● laitue iceberg ● cornichons ● oignons caramélisés ● sauce signature',
+  //       price: 9.90,
+  //       imgName: 'rosa'
+  //     }
+  //   ]
+  // };
 
   slideOpts = {
     spaceBetween: 15,
     slidesPerView: 'auto',
   };
-
-  showBasket = false;
 
   ngOnInit() {
   }
